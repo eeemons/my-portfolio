e0a7c028a4c7627d698fbe9e7f7689282e1c80d9
Maksudul Haque <e01716@gmail.com>
2022-06-20 01:10:07 +0600

    first commit

---
+++ src/startingCard.js
@@ -0,0 +1,11 @@
+import style from "./startingCard.module.css";
+function startingCard() {
+  return (
+    <div className={style.container}>
+      <div className={style.card}>
+        <h2>Maksudul Haque</h2>
+      </div>
+    </div>
+  );
+}
+export default startingCard;
