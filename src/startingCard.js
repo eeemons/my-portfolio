5479ad73b27ef32a23afaae2df2ec87a0589f0ca
Maksudul Haque <e01716@gmail.com>
2022-06-20 16:40:29 +0600

    flapping screen added

--- src/startingCard.js
+++ src/startingCard.js
@@ -2,8 +2,18 @@ import style from "./startingCard.module.css";
 function startingCard() {
   return (
     <div className={style.container}>
-      <div className={style.card}>
-        <h2>Maksudul Haque</h2>
+      <div className={style.cardContainer}>
+        <div className={style.content}>
+          <h1>Maksudul Haque</h1>
+          <h3>
+            A frontend web developer, recently graduated from American
+            International University. Eager to learn and making new things
+            happen through technology. Making innovative ideas and bringing them
+            alive is my priority.
+          </h3>
+          <a href="https://www.facebook.com/eeemons">Learn more</a>
+        </div>
+        <div className={style.flap}></div>
       </div>
     </div>
   );
