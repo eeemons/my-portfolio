fc043320235cbe72062cfaa20f413dfbc371ffb4
Maksudul Haque <e01716@gmail.com>
2022-06-23 17:38:40 +0600

    added router

--- src/startingCard.js
+++ src/startingCard.js
@@ -1,4 +1,8 @@
 import style from "./startingCard.module.css";
+import cssIcon from "./icons/css.png";
+import htmlIcon from "./icons/html.png";
+import vueIcon from "./icons/vue.png";
+import reactIcon from "./icons/react.png";
 function startingCard() {
   return (
     <div className={style.container}>
@@ -11,6 +15,14 @@ function startingCard() {
             happen through technology. Making innovative ideas and bringing them
             alive is my priority.
           </h3>
+          <h4>My skills </h4>
+          <div className={style.skill}>
+            <img src={cssIcon} alt="css"></img>
+            <img src={htmlIcon} alt="html"></img>
+            <img src={vueIcon} alt="vue"></img>
+            <img src={reactIcon} alt="react"></img>
+          </div>
+          <br></br>
           <a href="https://www.facebook.com/eeemons">Learn more</a>
         </div>
         <div className={style.flap}></div>
