8de0b8d7e3ecb58e22f5c14c6bc5f645fefc3426
Maksudul Haque <e01716@gmail.com>
2022-07-26 20:38:40 +0600

    Homepage update

--- src/homePage.js
+++ src/homePage.js
@@ -1,19 +1,72 @@
-import Education from "./education";
-import Footer from "./footer";
 import style from "./homePage.module.css";
+import Navbar from "./navbar";
+import UserDummy from "./icons/Designer.jpg";
 
 function homePage() {
   return (
     <div className={style.container}>
+      <Navbar></Navbar>
       <div className={style.mainContent}>
-        <h1>Maksudul Haque</h1>
-        <article>
-          Looking to make a name for myself, excelling at my work as a frontend
-          developer. Being a world class frontend developer is my ambition and I
-          would like to achieve the goal as soon as possible.
-        </article>
-        <Education />
-        <Footer />
+        <div className={style.column}>
+          <section>
+            <div className={style.hello}>
+              <p className={style.greetings}>Hello I am!</p>
+              <button>Web Developer</button>
+            </div>
+            <p className={style.greetings}>Maksudul Haque</p>
+          </section>
+
+          <section>
+            <div className={style.moto}>
+              <p>
+                I design website and make it real. I am passionate and hard
+                worker.
+              </p>
+            </div>
+          </section>
+
+          <section className={style.btnGroup}>
+            <button className={style.btnHire}>Hire Me</button>
+            <button className={style.btnProject}>Projects &#x2197;</button>
+          </section>
+
+          <section className={style.clientSection}>
+            <p className={style.number}>84+</p>
+            <p className={style.clientSectionPara}>
+              Clients on work world wide
+            </p>
+          </section>
+
+          <section className={style.contact}>
+            <div className={style.clientNumber}>
+              <p className={style.number}>572</p>
+              <p className={style.clientSectionPara}>Projects Done</p>
+            </div>
+            <div className={style.mail}>
+              <p>Contact</p>
+              <p>eeemons@live.com</p>
+              <p>01684683581</p>
+            </div>
+          </section>
+
+          <section className={style.footer}>
+            <div className={style.footerColumn}>
+              <p>Project</p>
+              <p>Statistics 2022</p>
+              <button>Know More</button>
+            </div>
+            <div className={style.footerColumn}>
+              <ul>
+                <li>Website Design</li>
+                <li>Mobile App Design</li>
+                <li>Brand Identity</li>
+              </ul>
+            </div>
+          </section>
+        </div>
+        <div className={style.column}>
+          <img src={UserDummy} alt="The user that created the website" />
+        </div>
       </div>
     </div>
   );
---
+++ src/navbar.js
@@ -0,0 +1,24 @@
+import style from "./navbar.module.css";
+function Navbar() {
+  return (
+    <div className={style.container}>
+      <section className={style.navName}>
+        <h2 className={style.name}>Maksudul Haque</h2>
+        <h2 className={style.dot}>.</h2>
+      </section>
+      <div className={style.links}>
+        <a href="#">Home</a>
+        <a href="#">Work</a>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Services</a>
+      </div>
+
+      <section>
+        <button>Let's Chat</button>
+      </section>
+    </div>
+  );
+}
+
+export default Navbar;
